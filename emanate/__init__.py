36685b52d6471a3faa994edfb09ad9544ca4d2d3
Ellen Marie Dash <me@duckie.co>
2018-06-01 12:46:59 -0400

    Actually expand file and directory paths.

--- emanate/__init__.py
+++ emanate/__init__.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Example usage:
 # TODO
 
 import argparse
 import sys
+from pathlib import Path
 from pprint import pprint
 
 class Emanate:
@@ -48,8 +49,16 @@ class Emanate:
         args = argparser.parse_args(argv[1:])
         return args
 
+    def path(self, raw_path):
+        return Path(raw_path).expanduser().resolve()
+
     def run(self, argv):
-        args = self.parse_args(argv)
+        args        = self.parse_args(argv)
+        destination = self.path(args.destination)
+        config      = self.path(args.config)
+
+        pprint(destination)
+        pprint(config)
         pprint(args)
         print("TODO.")
 
