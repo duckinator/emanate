a3709958be7b602c75af1baf7ab4cf5d7f80bb76
Ellen Marie Dash <me@duckie.co>
2018-06-02 12:19:22 -0400

    wheeeeeeeee

--- emanate/__init__.py
+++ emanate/__init__.py
@@ -4,12 +4,16 @@
 # TODO
 
 import argparse
-import sys
+from fnmatch import fnmatch
+import functools
+import json
+import os
 from pathlib import Path
+import sys
 from pprint import pprint
 
 class Emanate:
-    DEFAULT_EXCLUDE = [
+    DEFAULT_IGNORE = [
             "*~",
             ".*~",
             ".*.sw?",
@@ -40,7 +44,7 @@ class Emanate:
                 help="Don't prompt before replacing a file.")
         argparser.add_argument("--config",
                 metavar="CONFIG_FILE",
-                default="emanate.yml",
+                default="emanate.json",
                 help="Configuration file to use.")
         argparser.add_argument("--version",
                 action="store_true",
@@ -49,17 +53,29 @@ class Emanate:
         args = argparser.parse_args(argv[1:])
         return args
 
-    def path(self, raw_path):
-        return Path(raw_path).expanduser().resolve()
+    def ignored_file(self, config, path_obj):
+        config_ignore = config.get("ignore", [])
+        patterns = self.DEFAULT_IGNORE
+        match = functools.partial(fnmatch, str(path_obj))
+        return any(map(match, patterns))
+
+    def valid_file(self, config, path_obj):
+        return path_obj.is_file() and not (".git" in path_obj.parts) \
+            and not self.ignored_file(config, path_obj)
 
     def run(self, argv):
-        args        = self.parse_args(argv)
-        destination = self.path(args.destination)
-        config      = self.path(args.config)
+        args    = self.parse_args(argv)
+        dest    = Path(args.destination).expanduser().resolve()
+        config  = json.loads(Path(args.config).read_text())
+        # validfn is a partially-applied variant of valid_file(),
+        # which has the first argument always set to `config`.
+        validfn = functools.partial(self.valid_file, config)
+        files   = list(filter(validfn, Path(".").iterdir()))
 
-        pprint(destination)
+        pprint(dest)
         pprint(config)
         pprint(args)
+        print(files)
         print("TODO.")
 
 if __name__ == '__main__':
