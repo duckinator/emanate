63a873d54b0d456adb15ca500b69744adba9a22c
Ellen Marie Dash <me@duckie.co>
2018-06-03 08:52:46 -0400

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='emanate',
     version='1.0.0',
-    description='Symlink files from one directory to another, similarly to Effuse and Stow.'
+    description='Symlink files from one directory to another, similarly to Effuse and Stow.',
     author='Ellen Marie Dash',
     author_email='me@duckie.co',
     url='https://github.com/duckinator/emanate',
