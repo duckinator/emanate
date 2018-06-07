9d5b16d29467715f0c4e3645b918224daf4de6f8
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-06-08 00:28:11 +0200

    tests: Make a directory_tree helper

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,32 @@
 import json
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from contextlib import contextmanager
 from emanate import main
+
+
+@contextmanager
+def directory_tree(**kwargs):
+    def mktree(path, obj):
+        assert isinstance(path, Path)
+        # File by content
+        if isinstance(obj, str):
+            with path.open(mode='w') as file:
+                file.write(obj)
+
+        # Directory
+        elif 'type' not in obj:
+            path.mkdir(exist_ok=True)
+            for name, child in obj.items():
+                mktree(path / name, child)
+
+        elif obj['type'] == 'link':
+            path.symlink_to(obj['target'])
+
+    with TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir)
+        mktree(tmpdir, kwargs)
+        yield tmpdir
 
 
 def test_config_relative_path():
@@ -17,21 +42,16 @@ def test_config_relative_path():
     This test then asserts that tmpdir/dest/foo exists, and that
     tmpdir/dest/emanate.json does not.
     """
-    with TemporaryDirectory() as tmpdir:
-        # Setup the test environment
-        tmpdir = Path(tmpdir)
-        src = tmpdir.joinpath('src')
-        dest = tmpdir.joinpath('dest')
-        src.mkdir()
-        src.joinpath('foo').touch()
-        dest.mkdir()
+    with directory_tree(
+            src={
+                'foo': '',
+                'emanate.json': json.dumps({'destination': '../dest'}),
+            },
+            dest={},
+    ) as tmpdir:
+        dest_foo = tmpdir / 'dest' / 'foo'
 
-        with src.joinpath('emanate.json').open(mode='w') as config:
-            json.dump({
-                'destination': '../dest',
-            }, config)
-
-        assert not dest.joinpath('foo').exists()
-        main(['--source', str(src)])
-        assert dest.joinpath('foo').samefile(src.joinpath('foo'))
-        assert not dest.joinpath('emanate.json').exists()
+        assert not dest_foo.exists()
+        main(['--source', str(tmpdir / 'src')])
+        assert dest_foo.samefile(tmpdir / 'src'  / 'foo')
+        assert not (tmpdir / 'dest' / 'emanate.json').exists()
